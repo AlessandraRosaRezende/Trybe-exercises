55edb3d1d25689c950799e4674b2e92e10402b3f
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-04-07 16:28:27 -0300

    Exercicio 2

--- bloco_8/higher-order-functions-do-javascript-es6_5/exercicio2.js
+++ bloco_8/higher-order-functions-do-javascript-es6_5/exercicio2.js
@@ -1,9 +1,20 @@
 const assert = require('assert');
 
 // escreva sum abaixo
-
+const sum = (...args) => {
+  return args.reduce((accumulator, current) => accumulator + current, 0);
+}
+console.log(sum(), 0);
 assert.strictEqual(sum(), 0);
+
+console.log(sum(1), 1);
 assert.strictEqual(sum(1), 1);
+
+console.log(sum(1, 2), 3);
 assert.strictEqual(sum(1, 2), 3);
+
+console.log(sum(1, 2, 3), 6);
 assert.strictEqual(sum(1, 2, 3), 6);
+
+console.log(sum(1, 2, 3, 4), 10);
 assert.strictEqual(sum(1, 2, 3, 4), 10);
